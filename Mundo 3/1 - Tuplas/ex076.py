f15542286c7a282d670f4fd0601aa3fcb8e88579
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-05-30 22:09:17 -0300

    Delete ex076.py

--- Mundo 3/1 - Tuplas/ex076.py
+++
@@ -1,9 +0,0 @@
-''' Faça um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final,
-mostre uma listagem de preços, organizando os dados em forma tabular. '''
-
-produtos = ('Geladeira', 2499.99, 'Fogão', 999.00, 'Cama de Solteiro', 599.00, 'Cama de Casal', 1199.89, 'Mesa de Jantar',
-            499.99, 'Micro-Ondas', 479.29)
-
-for i in range (0, len(produtos)):
-    if i % 2 == 0:
-        print(f'{produtos[i]} - R$ {produtos[i+1]:.2f}')
